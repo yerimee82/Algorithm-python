5e02e115024f5fedb0bcf182e774c98600795b3a
Kim, Yerim <yerim343@naver.com>
2024-03-12 22:38:07 +0900

    sec5. 1번 수정

--- Section5/가장 큰 수.py
+++ Section5/가장 큰 수.py
@@ -1,7 +1,7 @@
 import sys
 sys.stdin = open("input.txt", "rt")
 
-num, n = map(int, input().split())
+num, t = map(int, input().split())
 
 arr = [int(n) for n in str(num)]
 stk = []
@@ -9,23 +9,37 @@ stk = []
 cnt = 0
 
 
+# for n in arr:
+#     if stk and cnt == t:
+#         break
+#     # 스택의 맨 마지막 값보다 넣을 값이 크면 pop 
+#     if stk and n > stk[-1]:
+#         while stk and n > stk[-1]:
+#             if cnt == t :
+#                 break
+#             stk.pop()
+#             cnt += 1
+#         stk.append(n)
+#     # 스택에 넣는다 
+#     else:
+#         stk.append(n)
+        
+
+# while cnt < t:
+#     stk.pop()
+#     cnt += 1
+
+
 for n in arr:
-    if cnt == n:
-        print(stk)
-    # 스택의 맨 마지막 값보다 넣을 값이 크면 pop 
-    if stk and n > stk[-1]:
-        while stk and n > stk[-1]:
-            if cnt == n :
-                print(stk)
-                break
-            stk.pop()
-            cnt += 1
-        stk.append(n)
-    # 스택에 넣는다 
-    else:
-        stk.append(n)
-    
-    
-    print(stk, cnt)
+    while stk and t > 0 and n > stk[-1]:
+        stk.pop()
+        t -= 1
+    stk.append(n)
+
+while t > 0:
+    stk.pop()
+    t-=1
+
+print(''.join(map(str, stk)))
     
 
